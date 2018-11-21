c8b239b8904352cea8e81ffd320879722035a6b4
upriypa <padma.priyanjith@pearson.com>
2018-11-21 07:51:28 +0530

    updated

--- vpc.tf
+++ vpc.tf
@@ -44,7 +44,7 @@ resource "aws_route_table" "padma-web-public-rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
+    gateway_id = "${aws_internet_gateway.padma-gw.id}"
   }
 
   tags {
